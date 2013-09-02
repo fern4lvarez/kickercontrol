3f5753bf469c586c8ae1eddbc55059c102ca3615
Fernando Álvarez <alvarez.calleja.fernando@gmail.com>
2013-09-02 17:09:19 +0200

    Weak statistics

--- app.rb
+++ app.rb
@@ -4,6 +4,19 @@ require 'sinatra'
 require 'sinatra/activerecord'
 require './environments'
 
+helpers do
+  def protected!
+    return if authorized?
+    headers['WWW-Authenticate'] = 'Basic realm="Restricted Area"'
+    halt 401, "Not authorized\n"
+  end
+
+  def authorized?
+    @auth ||=  Rack::Auth::Basic::Request.new(request.env)
+    @auth.provided? and @auth.basic? and @auth.credentials and @auth.credentials == ['admin', 'kickit']
+  end
+end
+
 
 class Player < ActiveRecord::Base
 end
@@ -16,6 +29,7 @@ class Game < ActiveRecord::Base
 end
 
 get "/" do
+	protected!
 	erb :match
 end
 
