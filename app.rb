32a333ce4e3cd7040989322896f0c694a9b75e55
Fernando Álvarez <alvarez.calleja.fernando@gmail.com>
2013-09-02 14:32:27 +0200

    Save game after playing

--- app.rb
+++ app.rb
@@ -21,5 +21,18 @@ end
 
 post "/result" do
 	@params = params[:post]
+	@team1backend = Player.where("name = ?", @params["team1backend"]).pluck("id").first
+	@team1frontend = Player.where("name = ?", @params["team1frontend"]).pluck("id").first
+	@team2backend = Player.where("name = ?", @params["team2backend"]).pluck("id").first
+	@team2frontend = Player.where("name = ?", @params["team2frontend"]).pluck("id").first
+
+	# puts @team1backend
+	Game.create({ :team1backend_id => @team1backend,
+								:team1frontend_id => @team1frontend,
+								:team2backend_id => @team2backend,
+								:team2frontend_id => @team2frontend,
+								:team1score => @params["team1score"],
+								:team2score => @params["team2score"]}
+								)
 	erb :result
 end
